d7547b25aeef8572ff4fd10bfc54085110841253
Toropolski_Bartosz <toropolski.bartosz@gmail.com>
2021-12-01 14:21:50 +0100

    first commit

---
+++ src/main/java/com/example/leisuretimemanagement/LeisureTimeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.leisuretimemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeisureTimeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeisureTimeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/leisuretimemanagement/LeisureTimeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leisuretimemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeisureTimeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
